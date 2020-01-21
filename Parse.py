22838932706feebf860122ae14c6c46e0ac554a5
gabejchambers <43391919+gabejchambers@users.noreply.github.com>
2020-01-21 13:24:25 -0500

    rough "I (keyword clause)" parsing
    if user has 'I' standalone in sentence, asks about everything after 'I'. i.e.
    User: "I sad"
    Eliza: "tell me more about sad"
    Needs polishing, more in comments

--- Parse.py
+++ Parse.py
@@ -6,5 +6,23 @@ def parse(userInput):
 import re
 
 def parse(userInput):
-    sentance = re.split(r'\s+', userInput)
-    return sentance[0]
+    tokens = re.split(r'\s+', userInput)
+
+    #finds an I and sends clause after it as keyword
+    #need to be done:
+    #if multiple I's (I think I am crazy), sends everything after the initial I. probly use that clause, but switch 'I's to 'you's -> 'tell me more about you crazy' not perfect idk
+    #doesnt recognize I'm
+    #verb after I needs to be handled. sometimes it would be okay to bring (I think I am crazy -> Why do you think you are crazy)
+    #   but often wouldnt make sense (I am sad !-> tell me more about am sad), actually, i think "am" is the only problomatic verb.
+
+    Itoken = re.search(r'I\s(.*?)$', userInput)
+    if Itoken is not None:
+        #print(Itoken.group(0)) #TESTING group(0) whole sentence
+        #print(Itoken.group(1)) #everything after the I
+        #print("^was taken out")
+        return Itoken.group(1)
+
+    return tokens[0] #only for testing
+
+
+
