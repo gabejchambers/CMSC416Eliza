090ba3940e953a1fa9ddc4e6b53f6e245fd847de
gabejchambers <43391919+gabejchambers@users.noreply.github.com>
2020-01-21 11:03:53 -0500

    split class
    split functions to classes

---
+++ Parse.py
@@ -0,0 +1,5 @@
+'''
+def parse(userInput):
+    keyword = use RegEx to ID a keyword #this needs to be robust as per instructions
+    return keyword
+'''
---
+++ Respond.py
@@ -0,0 +1,9 @@
+'''
+def respond(keyword):
+    #use switch or if statment to pick a response template
+    #insert keyword into template
+    #construct edgecase cases like gibberish, complicated user input, or first couple messages
+    return chosen response, with keyword inserted
+
+'''
+
